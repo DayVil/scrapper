f1f313551b2e7cffff8e1b8af365c6135a9f5f1c
Yann Bernard <yannbernard9@icloud.com>
2023-08-02 01:17:43 +0200

    sources

--- main.go
+++ main.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/gocolly/colly"
+	"github.com/DayVil/scrapper/src/config"
 )
 
 func main() {
-	c := colly.NewCollector()
+	cfg, err := config.NewCfg()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	c.Visit("http://go-colly.org/")
+	for _, line := range cfg.HttpSources {
+		fmt.Println(line)
+	}
 }
