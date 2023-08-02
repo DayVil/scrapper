5cacce3671f76d0878ee4c2ce8e090790ad28c11
Yann Bernard <yannbernard9@icloud.com>
2023-08-02 16:16:08 +0200

    remove duplicates from proxy list

--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -3,11 +3,12 @@ package proxy
 import (
 	"os"
 	"strings"
+
 	"github.com/DayVil/scrapper/src/scrape"
 )
 
 func getUrlSources() ([]string, error) {
-	var httpSources []string
+	httpSources := make([]string, 0)
 	content, err := os.ReadFile("./config/websource/http.txt")
 	if err != nil {
 		return nil, err
@@ -18,17 +19,32 @@ func getUrlSources() ([]string, error) {
 	return httpSources, nil
 }
 
+func removeDuplicateEntries(addrs []string) []string {
+	bucket := make(map[string]bool)
+	clean := make([]string, 0)
+
+	for _, entry := range addrs {
+		if ok := bucket[entry]; !ok {
+			bucket[entry] = true
+			clean = append(clean, entry)
+		}
+	}
+
+	return clean
+}
+
 func GetProxys() ([]string, error) {
 	scrapingSites, err := getUrlSources()
 	if err != nil {
 		return nil, err
 	}
 
-	var proxies []string
+	proxies := make([]string, 0)
 	for _, site := range scrapingSites {
 		proxyList := scrape.GetProxyListIpV4(site)
 		proxies = append(proxies, proxyList...)
 	}
+	proxies = removeDuplicateEntries(proxies)
 
 	return proxies, nil
 }
