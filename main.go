cd4359c6f29b391820809c3cd1a157b5472c7cba
Yann Bernard <yannbernard9@icloud.com>
2023-08-07 23:26:15 +0200

    refactoring

--- main.go
+++ main.go
@@ -9,17 +9,14 @@ import (
 )
 
 func main() {
-	// startTime := time.Now()
 	timeout := time.Second * 20
 	proxies, err := proxy.GetProxys("./config/websource/http.txt", "https://www.amazon.de/", 3, timeout)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	// elapsed := time.Since(startTime)
 
 	printProxys(proxies)
-	// fmt.Println("\nTime taken: " + elapsed.String())
 }
 
 func printProxys(proxyList []string) {
